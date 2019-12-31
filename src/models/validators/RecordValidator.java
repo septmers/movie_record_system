b6ed831642a037a1c4189992fc47ab89d85fefbc
nanami <sept.mers1004@gmail.com>
2019-12-31 15:28:04 +0900

    Modified RecordValidator

--- src/models/validators/RecordValidator.java
+++ src/models/validators/RecordValidator.java
@@ -7,20 +7,20 @@ import models.Record;
 
 public class RecordValidator {
 
-    public static List<String> validate(Record report){
+    public static List<String> validate(Record record){
         List<String>errors = new ArrayList<String>();
 
-        String title_error = validateTitle(report.getTitle());
+        String title_error = validateTitle(record.getTitle());
         if(! title_error.equals("")){
             errors.add(title_error);
         }
 
-        String impression_error = validateImpression(report.getImpression());
+        String impression_error = validateImpression(record.getImpression());
         if(! impression_error.equals("")){
             errors.add(impression_error);
         }
 
-        String mylife_error = validateMylife(report.getMylife());
+        String mylife_error = validateMylife(record.getMylife());
         if(! mylife_error.equals("")){
             errors.add(mylife_error);
         }
@@ -30,21 +30,21 @@ public class RecordValidator {
 
     private static String validateTitle(String title){
         if(title == null || title.equals("")){
-            return "タイトルを入力してください。";
+            return "Please type the TITLE!";
         }
         return "";
     }
 
     private static String validateImpression(String impression){
         if(impression == null || impression.equals("")){
-            return "感想を入力してください。";
+            return "Please type the IMPRESSION!";
         }
         return "";
     }
 
     private static String validateMylife(String mylife){
         if(mylife == null || mylife.equals("")){
-            return "人生録を入力してください。";
+            return "Please type the MY LIFE!";
         }
         return "";
     }
