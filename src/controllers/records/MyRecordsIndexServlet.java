55762fbab7c8bea4c808bf6c7ae239ffbc79bccb
nanami <sept.mers1004@gmail.com>
2019-12-31 15:33:47 +0900

    Create MyRecordsIndexServlet

---
+++ src/controllers/records/MyRecordsIndexServlet.java
@@ -0,0 +1,41 @@
+package controllers.records;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Record;
+import models.User;
+import utils.DBUtil;
+
+@WebServlet("/myrecords/index")
+public class MyRecordsIndexServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public MyRecordsIndexServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        User login_user = (User)request.getSession().getAttribute("login_user");
+        Integer user_id = login_user.getId();
+
+        List <Record> records = em.createNamedQuery("getMyRecords", Record.class)
+                                  .setParameter("user_id", user_id)
+                                  .getResultList();
+
+        request.setAttribute("records", records);
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/records/my_index.jsp");
+        rd.forward(request, response);
+    }
+
+}
